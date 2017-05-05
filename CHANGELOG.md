36ed9f4f4629795e9c87616f4bf4fab36929c6b8
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2017-05-05 16:16:25 -0500

    updated to use new sinon (v2) format

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 1.1.0
+* updated to use new sinon (v2) format
+
+# 1.0.1 Baseline
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function proptype_error_catcher (sandbox) {
-  sandbox.stub(console, 'error', function (message) {
+  sandbox.stub(console, 'error').callsFake(function (message) {
     if (!message.includes('Failed prop type')) return;
     throw new Error(message);
   });
