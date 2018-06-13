c96898036edd0c2c9aed1dc95f6a5ea9a29f2aeb
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2018-06-13 12:40:59 -0500

    fixed behavior in situations where it is already wrapped

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-'use strict';
+'use strict'
 
 function proptype_error_catcher (sandbox) {
+  const stub = (console.error.restore) ? console.error : sandbox.stub(console, 'error')
+
   sandbox.stub(console, 'error').callsFake(function (message) {
-    if (!message.includes('Failed prop type')) return;
-    throw new Error(message);
-  });
+    if (!message.includes('Failed prop type')) return
+    throw new Error(message)
+  })
 }
 
-module.exports = proptype_error_catcher;
+module.exports = proptype_error_catcher
